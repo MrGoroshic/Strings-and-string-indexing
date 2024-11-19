4b7d1b7fb64ed7237e58460b1531d1f089a7397d
Ilya <126606689+MrGoroshic@users.noreply.github.com>
2024-11-19 09:46:41 +0300

    Add files via upload

--- Strings_and_string_indexing.py
+++ Strings_and_string_indexing.py
@@ -1,6 +1,14 @@
-example = "Топинамбур"
+example = "Transmission"
 print(example[0])
 print(example[-1])
-print(example[5:])
+print(example[7:])
 print(example[::-1])
 print(example[1::2])
+
+print()
+example1 = "Топинамбур"
+print(example1[0])
+print(example1[-1])
+print(example1[5:])
+print(example1[::-1])
+print(example1[1::2])
